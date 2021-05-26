bb749d7244e3dc7d95f9cddb67c4e3956374e6ba
MANT-i-S <mantis.s95@gmail.com>
2021-05-26 11:24:28 -0700

    Manor bugs fixed, prepare for segue implemented, working on details storyboard and details view controller

--- Event-Monitor/DetailsController.swift
+++ Event-Monitor/DetailsController.swift
@@ -8,14 +8,35 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
-
+    
+    var detailsTitleText = String()
+    var detailsImage = UIImage()
+    var detailsTimeDateText = String()
+    var detailsLocationText = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let label = UILabel()
+        label.numberOfLines = 3
+        label.font = UIFont.boldSystemFont(ofSize: 16.0)
+        label.textAlignment = .center
+        label.text = detailsTitleText
+        label.lineBreakMode = .byWordWrapping
+        self.navigationItem.titleView = label
+        self.navigationItem.rightBarButtonItem?.image = UIImage(systemName: "heart.fill")
+        detailsImageLabel.image = detailsImage
+        detailsDateTimeLabel.text = detailsTimeDateText
+        detailsLocationLabel.text = detailsLocationText
         // Do any additional setup after loading the view.
     }
 
     @IBOutlet weak var detailsTitleLabel: UILabel!
     @IBOutlet weak var detailsImageLabel: UIImageView!
+    @IBOutlet weak var detailsDateTimeLabel: UILabel!
+    @IBOutlet weak var detailsLocationLabel: UILabel!
     
+    @IBAction func favoriteButtonTouch(_ sender: UIBarButtonItem) {
+        
+    }
 }
 
--- Event-Monitor/EventsTableViewController.swift
+++ Event-Monitor/EventsTableViewController.swift
@@ -46,10 +46,10 @@ class EventsTableViewController: UITableViewController, UISearchBarDelegate {
         let searchRequest = searchText.replacingOccurrences(of: " ", with: "+")
         eventMonitor.searchRequest = "q=" + searchRequest
         eventMonitor.page = 1
+        self.eventMonitor.events.removeAll()
         print("Text from searchbar is \(searchText)")
-        let textBackgroundQueue = DispatchQueue.global(qos: .userInitiated)
+        let textBackgroundQueue = DispatchQueue.global(qos: .userInteractive)
             textBackgroundQueue.async {
-                self.eventMonitor.events.removeAll()
                 self.eventMonitor.getData()
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
@@ -57,26 +57,47 @@ class EventsTableViewController: UITableViewController, UISearchBarDelegate {
             }
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let cell = sender as? EventTableViewCell,
-           let indexPath = tableView.indexPath(for: cell),
-           let seguedToMVC = segue.destination as? DetailsViewController {
-            //seguedToMVC.detailsTitleLabel.text = eventMonitor.events[indexPath.row].title
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {//TODO Figure out how segue from table to details pasing information.
+        if let identifier = segue.identifier {
+            switch identifier {
+            case "EventCellSegue":
+                if let cell = sender as? EventTableViewCell,
+                   let indexPath = tableView.indexPath(for: cell),
+                   let seguedToMVC = segue.destination as? DetailsViewController {
+                    seguedToMVC.detailsTitleText = eventMonitor.events[indexPath.row].title
+                    if let imageData = eventMonitor.events[indexPath.row].image {
+                        seguedToMVC.detailsImage = UIImage(data: imageData)!
+                    } else {
+                        seguedToMVC.detailsImage = UIImage(named: "noImagePlaceholder")!
+                    }
+                    seguedToMVC.detailsTimeDateText = eventMonitor.events[indexPath.row].displayDateTime!
+                    seguedToMVC.detailsLocationText = eventMonitor.events[indexPath.row].displayLocation
+                }
+            default: break
+            }
         }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell", for: indexPath)
         
+        if eventMonitor.events.isEmpty {
+            self.eventMonitor.getData()
+        }
+        
         if let myTVC = cell as? EventTableViewCell {
             print("self.eventMonitor.events.count = \(self.eventMonitor.events.count)")
             myTVC.titleLabel?.text = eventMonitor.events[indexPath.row].title
             myTVC.locationLabel?.text = eventMonitor.events[indexPath.row].displayLocation
             myTVC.dateTimeLabel?.text = eventMonitor.events[indexPath.row].displayDateTime
-            myTVC.imageLabel?.image = UIImage(data: eventMonitor.events[indexPath.row].image!)
+            if let imageData = eventMonitor.events[indexPath.row].image {
+                myTVC.imageLabel?.image = UIImage(data: imageData)
+            } else {
+                myTVC.imageLabel?.image = UIImage(named: "noImagePlaceholder")
+            }
             print("Index path row = \(indexPath.row)")
             print("self.eventMonitor.events.count = \(self.eventMonitor.events.count)")
-            if indexPath.row >= self.eventMonitor.events.count - 1 {
+            if indexPath.row == self.eventMonitor.events.count - 1 {
                 print("self.eventMonitor.events.count = \(self.eventMonitor.events.count)")
                 eventMonitor.page += 1
                 let textBackgroundQueue = DispatchQueue.global(qos: .userInitiated)
